b54e4d242f404fdec240b6dcb4b7c89b59ae85b5
Michelle Tan <mtan22@calpoly.edu>
2023-04-23 17:51:01 -0700

    ok

--- src/api/conversations.py
+++ src/api/conversations.py
@@ -50,7 +50,7 @@ def add_conversation(movie_id: int, conversation: ConversationJson):
         raise HTTPException(status_code=404, detail="characters are the same.")
     if movie_id not in db.movies:
         raise HTTPException(status_code=404, details="movie is not found.")
-    if char1 not in db.characters or char2 not in db.characters:
+    if convo_char_1 not in db.characters or convo_char_2 not in db.characters:
         raise HTTPException(status_code=404, details="character is not found.")
     if movie_id != char1[movie_id] or movie_id != char2[movie_id]:
         raise HTTPException(status_code=404, details="characters are not part of the referenced movie.")

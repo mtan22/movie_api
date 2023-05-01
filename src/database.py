bb192f99597d49c24bdd3a17bf5a07351ef87224
Michelle Tan <mtan22@calpoly.edu>
2023-04-30 22:34:26 -0700

    fix

--- src/database.py
+++ src/database.py
@@ -7,20 +7,6 @@ import dotenv
 from sqlalchemy import create_engine
 import sqlalchemy
 
-# # DO NOT CHANGE THIS TO BE HARDCODED. ONLY PULL FROM ENVIRONMENT VARIABLES.
-dotenv.load_dotenv()
-supabase_api_key = os.environ.get("SUPABASE_API_KEY")
-supabase_url = os.environ.get("SUPABASE_URL")
-
-if supabase_api_key is None or supabase_url is None:
-    raise Exception(
-        "You must set the SUPABASE_API_KEY and SUPABASE_URL environment variables."
-    )
-
-supabase: Client = create_client(supabase_url, supabase_api_key)
-
-sess = supabase.auth.get_session()
-
 def database_connection_url():
     dotenv.load_dotenv()
     DB_USER: str = os.environ.get("POSTGRES_USER")
@@ -32,8 +18,6 @@ def database_connection_url():
 
 engine = sqlalchemy.create_engine(database_connection_url())
 
-conn = engine.connect()
-
 metadata_obj = sqlalchemy.MetaData()
 
 conversations = sqlalchemy.Table("conversations", metadata_obj, autoload_with=engine)
@@ -43,6 +27,13 @@ lines = sqlalchemy.Table("lines", metadata_obj, autoload_with=engine)
 movies = sqlalchemy.Table("movies", metadata_obj, autoload_with=engine)
 
 characters = sqlalchemy.Table("characters", metadata_obj, autoload_with=engine)
+
+db = {
+    "movies": movies,
+    "characters":characters,
+    "conversations":conversations,
+    "lines":lines,
+}
 
 # # TODO: Below is purely an example of reading and then writing a csv from supabase.
 # # You should delete this code for your working example.
